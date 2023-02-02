703171f3e51d51707ff86e5a2c42238e8a7bdb26
JoshSauce1 <joshuaabreu16@gmail.com>
2023-02-02 18:26:44 -0500

    App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import './App.js'
 
 function App() {
+
+  var url = 'https://api.edamam.com/api/recipes/v2/0123456789abcdef0123456789abcdef?app_id=YOUR_APP_ID&app_key=YOUR_APP_KEY&type=public'
+
   return (
     <div className="App">
       <h1>Hello Josh</h1>
---
+++ src/key.js
@@ -0,0 +1,2 @@
+const app_key = "262db0e905563ad204920dd9bbefc479";
+const app_id = "da130918";
