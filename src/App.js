e54f63bdf77fda395d3579f26b5f816b144d99c6
JoshSauce1 <joshuaabreu16@gmail.com>
2023-02-06 11:48:14 -0500

    API setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 
 import './App.css';
-import './App.js'
+import './App.js';
+import Axios from 'axios';
 
 function App() {
+  const app_key = "2f4a33fce4f4959bc8cd6c7777b0ca7a";
+  const app_id = "da130918";
 
-  var url = 'https://api.edamam.com/api/recipes/v2/0123456789abcdef0123456789abcdef?app_id=YOUR_APP_ID&app_key=YOUR_APP_KEY&type=public'
+  var url = `https://api.edamam.com/api/recipes/v2?type=public&app_id=da130918&app_key=2f4a33fce4f4959bc8cd6c7777b0ca7a&diet=high-protein&health=alcohol-free
+  `
+
+
+  async function  getRecipes(){
+    var result = await Axios.get(url);
+    console.log(result.data);
+  }
+
 
   return (
     <div className="App">
-      <h1>Hello Josh</h1>
+      <h1 onClick={getRecipes}>Hello Josh</h1>
     </div>
   );
 }
