d39789424fc7e1b19833c7f59bbef878c90479fa
JoshSauce1 <joshuaabreu16@gmail.com>
2023-02-16 13:47:09 -0500

    CSS changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import './App.js';
 import  RecipeTile  from './RecipeTile.js';
 import Axios from 'axios';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [query, setquery] = useState("");
   const [recipes, setrecipes] = useState([])
   const [healthLabel, sethealthLabel] = useState(["vegan"])
+
+
 
   const app_key = "2f4a33fce4f4959bc8cd6c7777b0ca7a";
   const app_id = "da130918";
@@ -29,12 +31,15 @@ function App() {
 
   return (
     <div className="app">
+      <div className = "app_header">
       <h1>Culinary Craze</h1>
+      <p> Powered by the Edemam Recipe Search API, enter any food ingriedient to see how it works!</p>
+      </div>
       <form className="app__searchForm" onSubmit={onSubmit}>
         <input 
         type="text" 
         className="app__input"
-        placeholder="enter ingriedient" 
+        placeholder="Enter Ingredient!" 
         value={query} onChange={(e) => setquery (e.target.value)} 
         />
         <input className = "app__submit" type="submit" value="Search" />
@@ -46,11 +51,13 @@ function App() {
           </select>
       </form>
 
+      <div className = "app_note"><p>*Note: The API only allows a maximum of 5 searches per minute</p></div>
       <div className = "app_Recipes">
         {recipes.map((recipe) => {
           return <RecipeTile recipe={recipe}/>;
         })}
       </div>
+
     </div>
   );
 }
--- src/RecipeTile.js
+++ src/RecipeTile.js
@@ -2,10 +2,13 @@ import './RecipeTile.css';
 import React from 'react';
 
 export  default function RecipeTile({recipe}) {
+  let ingredients = recipe["recipe"]["ingredients"];
   return (
         <div className="recipeTile">
-        <img className="recipe_image" src={recipe["recipe"]["image"]}></img>
         <p className="recipe_label">{recipe["recipe"]["label"]}</p>
+        <img className="recipe_image" src={recipe["recipe"]["image"]}></img>
     </div>
+
+  
     );
 }
